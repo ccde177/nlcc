c16941a5be7db9ecf7af7ac3c5efd099803d26ed
ccde177 <sammorow@gmail.com>
2024-10-22 02:18:00 +0300

    Add missing cursor files

---
+++ src/lexer/cursor.rs
@@ -0,0 +1,35 @@
+use std::str::Chars;
+
+#[derive(Clone)]
+pub struct Cursor<'a> {
+    chars: Chars<'a>,
+}
+
+impl<'a> Cursor<'a> {
+    pub fn new(s: &'a str) -> Self {
+        let chars = s.chars();
+        Self {
+            chars
+        }
+    }
+
+    pub fn peek(&self) -> Option<char> {
+        self.chars.clone().next()
+    }
+
+    pub fn peek_2nd(&self) -> Option<char> {
+        // cheap to clone
+        let mut chars = self.chars.clone();
+        //faster than .nth(1)
+        chars.next();
+        chars.next()
+    }
+
+    pub fn take(&mut self) -> Option<char> {
+        self.chars.next()
+    }
+
+    pub fn as_str(&self) -> &'a str {
+        self.chars.as_str()
+    }
+}
---
+++ src/parser/cursor.rs
@@ -0,0 +1,69 @@
+use crate::lexer::Token;
+use crate::parser::{ParseError, Result};
+
+#[derive(Debug)]
+pub struct Cursor<'a> {
+    tokens: &'a [Token],
+    position: usize
+}
+
+impl<'a> Cursor<'a> {
+    pub fn new(tokens: &'a [Token]) -> Self {
+        Self {
+            tokens,
+            position: 0
+        }
+    }
+
+    pub fn peek(&self) -> Option<&Token> {
+        self.tokens.get(self.position)
+    }
+
+    pub fn bump(&mut self) {
+        self.position += 1;
+    }
+
+    pub fn bump_if(&mut self, t: &Token) -> bool {
+        let condition = self.peek() == Some(t);
+        if condition {
+            self.bump();
+        }
+        condition
+    }
+    
+    pub fn at_end(&self) -> bool {
+        self.position >= self.tokens.len()
+    }
+
+    pub fn peek_nth(&self, n: usize) -> Option<&Token> {
+        self.tokens.get(self.position + n)
+    }
+
+    pub fn expect(&mut self, t: &Token) -> Result<()> {
+        let next = self.next_or_error()?;
+        if next == t {
+            Ok(())
+        } else {
+            Err(ParseError::ExpectedButGot(t.clone(), next.clone()))
+        }
+    }
+
+    pub fn peek_nth_or_error(&self, n: usize) -> Result<&Token> {
+        self.peek_nth(n)
+            .ok_or(ParseError::UnexpectedEof)
+    }
+
+    pub fn peek_or_error(&mut self) -> Result<&Token> {
+        self.peek()
+            .ok_or(ParseError::UnexpectedEof)
+    }
+
+    pub fn next_or_error(&mut self) -> Result<&Token> {
+        let next = self
+            .tokens
+            .get(self.position)
+            .ok_or(ParseError::UnexpectedEof)?;
+        self.position += 1;
+        Ok(next)
+    }
+}
