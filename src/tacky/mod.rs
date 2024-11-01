e38735c249aa27263e9c81173144a0a966fe02a8
ccde177 <sammorow@gmail.com>
2024-11-01 23:13:41 +0300

    Add tacky support for types

--- src/tacky/mod.rs
+++ src/tacky/mod.rs
@@ -2,7 +2,7 @@
 mod tacky_tests;
 
 use crate::ast::*;
-use crate::semantic_analysis::SYM_TABLE;
+use crate::semantic_analysis::{StaticInit, SYM_TABLE};
 
 use std::sync::atomic::{AtomicUsize, Ordering};
 
@@ -26,7 +26,8 @@ pub struct TFunction {
 pub struct StaticVariable {
     pub name: Identifier,
     pub global: bool,
-    pub init: i64,
+    pub init: StaticInit,
+    pub var_type: Type,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -37,6 +38,8 @@ pub enum TopLevelItem {
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum TInstruction {
+    Truncate(TValue, TValue),   //(src, dst)
+    SignExtend(TValue, TValue), // (src,dst)
     Return(TValue),
     Unary(TUnaryOp, TValue, TValue),
     Binary(TBinaryOp, TValue, TValue, TValue),
@@ -74,7 +77,7 @@ pub enum TBinaryOp {
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum TValue {
-    Constant(u64),
+    Constant(AstConst),
     Var(Identifier),
 }
 
@@ -158,14 +161,18 @@ impl From<AstBinaryOp> for TBinaryOp {
 }
 
 fn emit_postfix_incdec(op: AstUnaryOp, exp: Exp, instructions: &mut TInstructions) -> TValue {
-    let one = TValue::Constant(1);
+    let t = exp.get_type().expect("Should have type after typechecking");
+    let one = AstConst::new(&t, 1).expect("Should always be Some");
+    let one = TValue::Constant(one);
     let op = if matches!(op, AstUnaryOp::PostfixIncrement) {
         TBinaryOp::Add
     } else {
         TBinaryOp::Substract
     };
     let original = emit_expression(exp, instructions);
-    let new_var = TValue::Var(get_uniq_name());
+
+    let new_var = new_tacky_var(t);
+
     let copy = TInstruction::Copy(original.clone(), new_var.clone());
     let modify = TInstruction::Binary(op, new_var.clone(), one, original);
     instructions.push(copy);
@@ -174,14 +181,18 @@ fn emit_postfix_incdec(op: AstUnaryOp, exp: Exp, instructions: &mut TInstruction
 }
 
 fn emit_prefix_incdec(op: AstUnaryOp, exp: Exp, instructions: &mut TInstructions) -> TValue {
-    let one = TValue::Constant(1);
+    let t = exp.get_type().expect("Should have type after typechecking");
+    let one = AstConst::new(&t, 1).expect("Should always be Some");
+    let one = TValue::Constant(one);
     let op = if matches!(op, AstUnaryOp::PrefixIncrement) {
         TBinaryOp::Add
     } else {
         TBinaryOp::Substract
     };
     let src = emit_expression(exp, instructions);
-    let dst = TValue::Var(get_uniq_name());
+
+    let dst = new_tacky_var(t);
+
     let modify = TInstruction::Binary(op, src.clone(), one, dst.clone());
     let copy = TInstruction::Copy(dst.clone(), src.clone());
     instructions.push(modify);
@@ -190,9 +201,12 @@ fn emit_prefix_incdec(op: AstUnaryOp, exp: Exp, instructions: &mut TInstructions
 }
 
 fn emit_unary(op: AstUnaryOp, exp: Exp, instructions: &mut TInstructions) -> TValue {
+    let t = exp
+        .get_type()
+        .expect("Should have type after type checking");
     let tacky_op = TUnaryOp::from(op);
     let src = emit_expression(exp, instructions);
-    let dst = TValue::Var(get_uniq_name());
+    let dst = new_tacky_var(t);
     let tacky_instruction = TInstruction::Unary(tacky_op, src, dst.clone());
     instructions.push(tacky_instruction);
     dst
@@ -201,9 +215,9 @@ fn emit_unary(op: AstUnaryOp, exp: Exp, instructions: &mut TInstructions) -> TVa
 fn emit_logical_and(src: Exp, dst: Exp, instructions: &mut TInstructions) -> TValue {
     let false_label = get_uniq_label();
     let label_end = get_uniq_label();
-    let result = TValue::Var(get_uniq_name());
-    let copy0 = TInstruction::Copy(TValue::Constant(0), result.clone());
-    let copy1 = TInstruction::Copy(TValue::Constant(1), result.clone());
+    let result = new_tacky_var(Type::Int);
+    let copy0 = TInstruction::Copy(TValue::Constant(AstConst::Int(0)), result.clone());
+    let copy1 = TInstruction::Copy(TValue::Constant(AstConst::Int(0)), result.clone());
     let jumpend = TInstruction::Jump(label_end.clone());
 
     let v1 = emit_expression(src, instructions);
@@ -226,9 +240,9 @@ fn emit_logical_and(src: Exp, dst: Exp, instructions: &mut TInstructions) -> TVa
 fn emit_logical_or(src: Exp, dst: Exp, instructions: &mut TInstructions) -> TValue {
     let true_label = get_uniq_label();
     let label_end = get_uniq_label();
-    let result = TValue::Var(get_uniq_name());
-    let copy0 = TInstruction::Copy(TValue::Constant(0), result.clone());
-    let copy1 = TInstruction::Copy(TValue::Constant(1), result.clone());
+    let result = new_tacky_var(Type::Int);
+    let copy0 = TInstruction::Copy(TValue::Constant(AstConst::Int(0)), result.clone());
+    let copy1 = TInstruction::Copy(TValue::Constant(AstConst::Int(1)), result.clone());
     let jumpend = TInstruction::Jump(label_end.clone());
 
     let v1 = emit_expression(src, instructions);
@@ -248,11 +262,18 @@ fn emit_logical_or(src: Exp, dst: Exp, instructions: &mut TInstructions) -> TVal
     result
 }
 
-fn emit_binary(op: AstBinaryOp, exp1: Exp, exp2: Exp, instructions: &mut TInstructions) -> TValue {
+fn emit_binary(
+    op: AstBinaryOp,
+    exp1: Exp,
+    exp2: Exp,
+    instructions: &mut TInstructions,
+    t: Type,
+) -> TValue {
     let v1 = emit_expression(exp1, instructions);
     let v2 = emit_expression(exp2, instructions);
-    let dst_name = get_uniq_name();
-    let dst = TValue::Var(dst_name);
+
+    let dst = new_tacky_var(t);
+
     let tacky_op = TBinaryOp::from(op);
     let tacky_instruction = TInstruction::Binary(tacky_op, v1, v2, dst.clone());
     instructions.push(tacky_instruction);
@@ -260,7 +281,9 @@ fn emit_binary(op: AstBinaryOp, exp1: Exp, exp2: Exp, instructions: &mut TInstru
 }
 
 fn emit_assignment(var: Exp, rhs: Exp, instructions: &mut TInstructions) -> TValue {
-    let Exp::Var(name) = var else { unreachable!() };
+    let UntypedExp::Var(name) = var.into() else {
+        unreachable!()
+    };
     let rhs = emit_expression(rhs, instructions);
     let var = TValue::Var(name);
     let copy = TInstruction::Copy(rhs, var.clone());
@@ -278,8 +301,15 @@ fn emit_conditional(cond: ConditionalExp, instructions: &mut TInstructions) -> T
     let e2 = get_uniq_label();
     let jz = TInstruction::JumpIfZero(c, e2.clone());
     instructions.push(jz);
+
+    let then_type = then
+        .get_type()
+        .expect("Should have type after type checking");
+
     let v1 = emit_expression(*then, instructions);
-    let result = TValue::Var(get_uniq_name());
+
+    let result = new_tacky_var(then_type);
+
     let copy = TInstruction::Copy(v1, result.clone());
     instructions.push(copy);
     let end = get_uniq_label();
@@ -295,13 +325,19 @@ fn emit_conditional(cond: ConditionalExp, instructions: &mut TInstructions) -> T
     result
 }
 
-fn emit_call(name: Identifier, args: Vec<Exp>, instructions: &mut TInstructions) -> TValue {
+fn emit_call(
+    name: Identifier,
+    args: Vec<Exp>,
+    instructions: &mut TInstructions,
+    ret_type: Type,
+) -> TValue {
     let args = args
         .into_iter()
         .map(|e| emit_expression(e, instructions))
         .collect();
-    let dst_name = get_uniq_name();
-    let dst = TValue::Var(dst_name);
+
+    let dst = new_tacky_var(ret_type);
+
     let tacky_call = TInstruction::FunCall {
         name,
         args,
@@ -311,25 +347,52 @@ fn emit_call(name: Identifier, args: Vec<Exp>, instructions: &mut TInstructions)
     dst
 }
 
-fn emit_expression(e: Exp, instructions: &mut TInstructions) -> TValue {
-    match e {
-        Exp::Call(name, args) => emit_call(name, args, instructions),
-        Exp::Unary(op @ (AstUnaryOp::PostfixIncrement | AstUnaryOp::PostfixDecrement), exp) => {
+fn new_tacky_var(t: Type) -> TValue {
+    let var_name = get_uniq_name();
+    SYM_TABLE.add_local_sym(var_name.clone(), t);
+    TValue::Var(var_name)
+}
+
+fn emit_cast(t: Type, e: Exp, instructions: &mut TInstructions) -> TValue {
+    let inner_type = e.get_type().expect("Should have type after typechecking");
+    let result = emit_expression(e, instructions);
+    if t == inner_type {
+        return result;
+    }
+    let dst = new_tacky_var(t.clone());
+    let cast_instr = if t == Type::Long {
+        TInstruction::SignExtend(result, dst.clone())
+    } else {
+        TInstruction::Truncate(result, dst.clone())
+    };
+    instructions.push(cast_instr);
+
+    dst
+}
+
+fn emit_expression(exp: Exp, instructions: &mut TInstructions) -> TValue {
+    use UntypedExp as UE;
+    let t = exp
+        .get_type()
+        .expect("Should have type after type checking");
+    let ue = exp.into();
+    match ue {
+        UE::Cast(casting_t, e) => emit_cast(casting_t, *e, instructions),
+        UE::Call(name, args) => emit_call(name, args, instructions, t),
+        UE::Unary(op @ (AstUnaryOp::PostfixIncrement | AstUnaryOp::PostfixDecrement), exp) => {
             emit_postfix_incdec(op, *exp, instructions)
         }
-        Exp::Unary(op @ (AstUnaryOp::PrefixIncrement | AstUnaryOp::PrefixDecrement), exp) => {
+        UE::Unary(op @ (AstUnaryOp::PrefixIncrement | AstUnaryOp::PrefixDecrement), exp) => {
             emit_prefix_incdec(op, *exp, instructions)
         }
-        Exp::Unary(op, exp) => emit_unary(op, *exp, instructions),
-        Exp::Binary(AstBinaryOp::LogicalAnd, src, dst) => {
-            emit_logical_and(*src, *dst, instructions)
-        }
-        Exp::Binary(AstBinaryOp::LogicalOr, src, dst) => emit_logical_or(*src, *dst, instructions),
-        Exp::Binary(op, exp1, exp2) => emit_binary(op, *exp1, *exp2, instructions),
-        Exp::Assignment(var, rhs) => emit_assignment(*var, *rhs, instructions),
-        Exp::Var(name) => TValue::Var(name.clone()),
-        Exp::Constant(u) => TValue::Constant(u),
-        Exp::Conditional(cond) => emit_conditional(cond, instructions),
+        UE::Unary(op, exp) => emit_unary(op, *exp, instructions),
+        UE::Binary(AstBinaryOp::LogicalAnd, src, dst) => emit_logical_and(*src, *dst, instructions),
+        UE::Binary(AstBinaryOp::LogicalOr, src, dst) => emit_logical_or(*src, *dst, instructions),
+        UE::Binary(op, exp1, exp2) => emit_binary(op, *exp1, *exp2, instructions, t),
+        UE::Assignment(var, rhs) => emit_assignment(*var, *rhs, instructions),
+        UE::Var(name) => TValue::Var(name.clone()),
+        UE::Constant(u) => TValue::Constant(u),
+        UE::Conditional(cond) => emit_conditional(cond, instructions),
     }
 }
 
@@ -355,7 +418,7 @@ fn emit_switch(switch: Switch, instructions: &mut TInstructions) {
     let label_end = format!("break_{label}");
     for case in cases {
         if let Some(value) = case.0 {
-            let cmp_result = TValue::Var(get_uniq_name());
+            let cmp_result = new_tacky_var(Type::Int);
             let is_equal = TInstruction::Binary(
                 TBinaryOp::IsEqual,
                 v.clone(),
@@ -581,11 +644,12 @@ fn emit_fundec(f: FunDec) -> Option<TFunction> {
         params,
         body,
         storage_class: _,
+        fun_type: _,
     } = f;
     if let Some(body) = body {
         let AstBlock { items } = body;
         let mut body = emit_block_items(items);
-        body.push(TInstruction::Return(TValue::Constant(0)));
+        body.push(TInstruction::Return(TValue::Constant(AstConst::Int(0))));
 
         let st_entry = SYM_TABLE.get_symbol(&name).expect("Should never fail");
         let f = TFunction {
@@ -603,6 +667,7 @@ fn emit_fundec(f: FunDec) -> Option<TFunction> {
 fn emit_toplevel_dec(dec: Declaration) -> Option<TopLevelItem> {
     match dec {
         Declaration::Fun(fundec) => emit_fundec(fundec).map(TopLevelItem::Fun),
+        // It will be processed later during emit_static_symbols
         Declaration::Var(_) => None,
     }
 }
@@ -610,11 +675,25 @@ fn emit_toplevel_dec(dec: Declaration) -> Option<TopLevelItem> {
 fn emit_static_symbols() -> Vec<TopLevelItem> {
     let mut defs = Vec::new();
     for symbol in SYM_TABLE.get_keys() {
-        let entry = SYM_TABLE.get_symbol(symbol).expect("Should always be Some");
+        let entry = SYM_TABLE
+            .get_symbol(&symbol)
+            .expect("Should always be Some");
         if let Some(init) = entry.get_init() {
+            let sym_type = entry.sym_type.clone();
+            let tentative_init = match sym_type {
+                Type::Int => StaticInit::Int(0),
+                Type::Long => StaticInit::Long(0),
+                _ => continue,
+            };
+            let init = init.get_static_init().unwrap_or(tentative_init);
             let name = symbol.to_owned();
             let global = entry.is_global();
-            let staticvar = StaticVariable { name, global, init };
+            let staticvar = StaticVariable {
+                name,
+                global,
+                init,
+                var_type: sym_type,
+            };
             defs.push(TopLevelItem::Var(staticvar));
         }
     }
