f2a7f0817a873e1abea277043ecb39cbc9c9feea
ccde177 <sammorow@gmail.com>
2024-11-04 17:26:42 +0300

    Downcase error messages

--- src/semantic_analysis/mod.rs
+++ src/semantic_analysis/mod.rs
@@ -48,46 +48,46 @@ impl fmt::Display for SemAnalysisError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::StaticFunctionRedeclaredNonStatic(name) => {
-                write!(f, "Static function {name} redeclared ad non-static")
+                write!(f, "static function {name} redeclared ad non-static")
             }
             Self::StorageIdInForInit(name) => write!(
                 f,
-                "Variable {name} has storage keyword in a for loop init expression"
+                "variable {name} has storage keyword in a for loop init expression"
             ),
             Self::ConflictingLinkage(name) => {
-                write!(f, "Conflicting linkage for identifier {name}")
+                write!(f, "conflicting linkage for identifier {name}")
             }
-            Self::InitOnExternVar(name) => write!(f, "Extern variable {name} has initializer"),
+            Self::InitOnExternVar(name) => write!(f, "extern variable {name} has initializer"),
             Self::NonConstantInit(name) => {
-                write!(f, "Global variable {name} has non-constant initializer")
+                write!(f, "global variable {name} has non-constant initializer")
             }
-            Self::IdentifierRedeclaration(id) => write!(f, "Redeclaration of a identifier {id}"),
+            Self::IdentifierRedeclaration(id) => write!(f, "redeclaration of a identifier {id}"),
             Self::LocalFunDefinition(name) => {
-                write!(f, "Attempt to define function{name} in local local context")
+                write!(f, "attempt to define function{name} in local local context")
             }
-            Self::VariableNotDeclared(id) => write!(f, "Unknown variable: {id}"),
-            Self::WrongLvalue(exp) => write!(f, "Wrong lvalue: {exp:?}"),
-            Self::LabelRedeclaration(name) => write!(f, "Label {name} redeclaration"),
-            Self::UnknownLabel(name) => write!(f, "Unknown label {name}"),
+            Self::VariableNotDeclared(id) => write!(f, "unknown variable: {id}"),
+            Self::WrongLvalue(exp) => write!(f, "wrong lvalue: {exp:?}"),
+            Self::LabelRedeclaration(name) => write!(f, "label {name} redeclaration"),
+            Self::UnknownLabel(name) => write!(f, "unknown label {name}"),
             Self::BreakOutsideOfLoop => write!(f, "break statement outside of loop"),
             Self::ContinueOutsideOfLoop => write!(f, "continue statement outside of loop"),
             Self::CaseNotInSwitch => write!(f, "case not in switch"),
             Self::NotAConstCase(exp) => {
-                write!(f, "Not a const expression in case: {exp:?}")
+                write!(f, "not a const expression in case: {exp:?}")
             }
             Self::DefaultNotInSwitch => write!(f, "default case not in switch"),
-            Self::DuplicateCase(case) => write!(f, "Duplicate case {case:?}"),
-            Self::UndeclaredFunction(s) => write!(f, "Call to undeclared function {s}"),
-            Self::DuplicateDeclaration(name) => write!(f, "Redeclaration of {name}"),
+            Self::DuplicateCase(case) => write!(f, "duplicate case {case:?}"),
+            Self::UndeclaredFunction(s) => write!(f, "call to undeclared function {s}"),
+            Self::DuplicateDeclaration(name) => write!(f, "redeclaration of {name}"),
             Self::IncompatibleFunDec(name) => {
-                write!(f, "Incompatible redeclaration of function {name}")
+                write!(f, "incompatible redeclaration of function {name}")
             }
-            Self::FunctionRedefinition(name) => write!(f, "Redefinition of a function {name}"),
-            Self::VariableCall(name) => write!(f, "Variable {name} called as a function"),
-            Self::FunctionNameAsVariable(name) => write!(f, "Function {name} used as variable"),
+            Self::FunctionRedefinition(name) => write!(f, "redefinition of a function {name}"),
+            Self::VariableCall(name) => write!(f, "variable {name} called as a function"),
+            Self::FunctionNameAsVariable(name) => write!(f, "function {name} used as variable"),
             Self::ExpectedArgsCountButGot(expected, got, name) => write!(
                 f,
-                "Function {name} called with {got} arguments, but expected {expected}"
+                "function {name} called with {got} arguments, but expected {expected}"
             ),
         }
     }
